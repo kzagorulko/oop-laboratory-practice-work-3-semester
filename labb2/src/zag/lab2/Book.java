b86c5825605d5ccab1aaddf38c4aed81a6cfff23
overpoweredKLEN <kostrock@gmail.com>
2018-10-16 23:14:48 +0300

    Добавил третье задание второй лабы

---
+++ labb2/src/zag/lab2/Book.java
@@ -0,0 +1,90 @@
+package zag.lab2;
+
+public class Book {
+    String author, title, publishing;
+    int year, pages;
+
+    public Book(int pages) {
+        this.pages  = pages;
+        this.author = "Unknown";
+        this.title  = "Unknown";
+        this.publishing = "Unknown";
+        this.year = 0;
+    }
+
+
+    public Book(String author, String title, int pages) {
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        this.publishing = "Unknown";
+        this.year = 0;
+    }
+
+    public Book(String author, String title, int year, int pages) {
+        this.author = author;
+        this.title = title;
+        this.year = year;
+        this.pages = pages;
+        this.publishing = "Unknown";
+    }
+
+    public Book(String author, String title, String publishing, int year, int pages) {
+        this.author = author;
+        this.title = title;
+        this.publishing = publishing;
+        this.year = year;
+        this.pages = pages;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", publishing='" + publishing + '\'' +
+                ", year=" + year +
+                ", pages=" + pages +
+                '}';
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublishing() {
+        return publishing;
+    }
+
+    public void setPublishing(String publishing) {
+        this.publishing = publishing;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+}
